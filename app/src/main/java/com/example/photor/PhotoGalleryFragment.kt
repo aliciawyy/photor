7b2c1d138e94e16d6671d7a1569fe6af4dcf47c5
alice <rainingilove@gmail.com>
2020-04-30 14:48:57 +0200

    Use picasso for image downloading

--- app/src/main/java/com/example/photor/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photor/PhotoGalleryFragment.kt
@@ -1,22 +1,17 @@
 package com.example.photor
 
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photor.data.FlickrPhotoItem
-import com.example.photor.data.ThumbnailDownloader
+import com.squareup.picasso.Picasso
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -24,15 +19,6 @@ class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
     private val photoViewModel: PhotoViewModel by activityViewModels()
-    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        thumbnailDownloader = ThumbnailDownloader(Handler()) {
-            photoHolder, bitmap -> photoHolder.bindImage(BitmapDrawable(resources, bitmap))
-        }
-        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,7 +32,6 @@ class PhotoGalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewLifecycleOwner.lifecycle.addObserver(thumbnailDownloader.viewLifecycleObserver)
         photoViewModel.photoList.observe(
             viewLifecycleOwner,
             Observer { photoList ->
@@ -57,21 +42,15 @@ class PhotoGalleryFragment : Fragment() {
         )
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        viewLifecycleOwner.lifecycle.removeObserver(thumbnailDownloader.viewLifecycleObserver)
+    private class PhotoHolder(private val photoImageView: ImageView)
+        : RecyclerView.ViewHolder(photoImageView) {
+        fun bindPhoto(photoItem: FlickrPhotoItem) {
+            Picasso.get().load(photoItem.url)
+                .into(photoImageView)
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        lifecycle.removeObserver(thumbnailDownloader.fragmentLifecycleObserver)
-    }
-
-    private class PhotoHolder(photoImageView: ImageView) : RecyclerView.ViewHolder(photoImageView) {
-        val bindImage: (Drawable) -> Unit = photoImageView::setImageDrawable
-    }
-
-    private inner class PhotoAdapter(private val photoList: List<FlickrPhotoItem>):
+    private class PhotoAdapter(private val photoList: List<FlickrPhotoItem>):
         RecyclerView.Adapter<PhotoHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
             val view = LayoutInflater.from(parent.context)
@@ -82,10 +61,8 @@ class PhotoGalleryFragment : Fragment() {
         override fun getItemCount(): Int = photoList.size
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-            val photoItem = photoList[position]
-            thumbnailDownloader.queueThumbnail(holder, photoItem.url)
+            holder.bindPhoto(photoList[position])
         }
-
     }
 }
 
